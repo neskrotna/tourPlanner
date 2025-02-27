672f260e01b5fab094a7995fa6e7c7183fa5cd62
gabriela <if23b101@technikum-wien.at>
2025-02-27 13:18:15 +0100

    Initial ui

---
+++ frontend/ui/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module at.technikum_wien.ui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens at.technikum_wien.ui to javafx.fxml;
+    exports at.technikum_wien.ui;
+}
